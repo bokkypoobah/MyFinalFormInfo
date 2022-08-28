195f173355b641bafa270b37428812217b10f310
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2022-08-29 06:57:44 +1000

    images

--- scripts/01_scrapeAddLayerTxs.js
+++ scripts/01_scrapeAddLayerTxs.js
@@ -82,8 +82,9 @@ async function doIt() {
 
       const name = tuple[0].toLowerCase().replace(/['\/ @]/g, '');
       let buffer = Buffer.from(tuple[2].substring(2), "hex");
-      let filename = "images/layer" + item.decodedData.args[0] + "-trait" + tupleIndex.toString().padStart(3, '0')+ "-" + name + '.' + tuple[1].replace(/^.*\//, '');
+      let filename = "images/layer" + item.decodedData.args[0] + "-trait" + tupleIndex.toString().padStart(3, '0') + "-" + name + '.' + tuple[1].replace(/^.*\//, '');
       // console.log(name + " => " + filename); //  + ' ' + buffer);
+      console.log('\n#### Layer ' + item.decodedData.args[0] + ' Trait ' + tupleIndex.toString().padStart(3, '0') + ' ' + tuple[0]);
       console.log('<kbd><img src="scripts/' + filename + '" width="100px" height="100px"/></kbd>'); //  + ' ' + buffer);
       fs.writeFile(filename, buffer, (err) => {
         if (err) return console.error(err);
